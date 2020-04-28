ab1c8d0bb81b4bce0a092469698346f473ef1ad1
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-04-28 00:37:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ class App < Sinatra::Base
   # Define your GET '/' route below and respond with "Hello, World!"
 
 get '/' do 
-    "Started my server using Shotgun!"
+    "Hello, World!"
   end
 
 end
